bc9900c6969d57fa14c40a4cfe52f3f663e6ad7e
RociNayara <rociosorianorives@gmail.com>
2020-03-14 20:42:49 +0100

    Header

---
+++ header.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Portal Docente</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="css/fontawesome-free-5.0.1/css/fontawesome-all.css" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Luckiest+Guy&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+
+<div class="super_container">
+   
+
+    <!-- Header -->
+	<header class="header">
+		<nav>
+           
+            <!-- Logo -->
+            <div class="logo">
+                <img src="images/logo2.png" alt="">
+            </div>
+                
+            <!-- Main Navigation -->
+            <div class="menu">
+                <input type="checkbox" id="btn-menu">
+                <label for="btn-menu" class="fas fa-bars"></label> <!--icono para menu-->
+                <ul class="menuLista">
+                    <li><a href="#">INICIO</a></li>
+                    <hr>
+                    <li><a href="#quien">NOSOTROS</a></li>
+                    <hr>
+                    <li><a href="#proyectos">PORTFOLIO</a></li>
+                    <hr>
+                    <li><a href="#">CONTACTO</a></li>
+                    <hr>
+                </ul>
+            </div>
+            
+        </nav>
+    </header>
+
+
+
+</div> <!--final div super_container-->
+
+
+
+
+</body>
+</html>
+
